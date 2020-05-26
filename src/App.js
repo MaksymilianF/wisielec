b34ae0517072a66ea86e8472f99456093fb3c919
Max <max@dietr.net>
2020-05-26 15:45:09 +0200

    Add images and more styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import s0 from './img/s0.jpg';
 
 function App() {
   return (
     <div className="App border">
       <div id='pswd' className="App-pswd">hasło do odgadnięcia</div>
       <div className='App-container'>
-        <div id='image' className="App-img border">obrazek</div>
+        <div id='image' className="App-img border">
+          <img src={s0} alt='wisielec' />
+        </div>
         <div id='alphabet' className="App-alph border">alfabet</div>
       </div>
     </div>
