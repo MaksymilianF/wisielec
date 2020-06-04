e54e3d408663c831b2fe59c8a651a802c1bfb1d8
Max <max@dietr.net>
2020-06-04 12:44:41 +0200

    Updates

---
+++ precache-manifest.60f834e0bece963b0334c96271a418c3.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be069425c9a4df017008bf076789bc38",
+    "url": "/wisielec/index.html"
+  },
+  {
+    "revision": "a700cc1c8d316da83889",
+    "url": "/wisielec/static/css/main.d7f89d38.chunk.css"
+  },
+  {
+    "revision": "41deabde3049a6649e77",
+    "url": "/wisielec/static/js/2.eef7ec12.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wisielec/static/js/2.eef7ec12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a700cc1c8d316da83889",
+    "url": "/wisielec/static/js/main.625c338b.chunk.js"
+  },
+  {
+    "revision": "4795f7b63991728381de",
+    "url": "/wisielec/static/js/runtime-main.e2c8a394.js"
+  },
+  {
+    "revision": "1db8c9491623f182f8f5934fb142e886",
+    "url": "/wisielec/static/media/s1.1db8c949.jpg"
+  },
+  {
+    "revision": "1d88e0ca637920a0ba6a519d44a16ef3",
+    "url": "/wisielec/static/media/s2.1d88e0ca.jpg"
+  },
+  {
+    "revision": "6aaed79316e0ba5bb6151929dc1c03de",
+    "url": "/wisielec/static/media/s3.6aaed793.jpg"
+  },
+  {
+    "revision": "8b2047c832c334b8ae20c4a846bb6686",
+    "url": "/wisielec/static/media/s4.8b2047c8.jpg"
+  },
+  {
+    "revision": "d1c2b3e919132ec37386a1eb07fae54a",
+    "url": "/wisielec/static/media/s5.d1c2b3e9.jpg"
+  },
+  {
+    "revision": "2126df30315d8f5ad0ba0ae01a9caf73",
+    "url": "/wisielec/static/media/s6.2126df30.jpg"
+  },
+  {
+    "revision": "e26b7cf6a08cdea34292a37eee8dd95d",
+    "url": "/wisielec/static/media/s7.e26b7cf6.jpg"
+  },
+  {
+    "revision": "6824e5f6e3043f8bde8d8519f968a863",
+    "url": "/wisielec/static/media/s8.6824e5f6.jpg"
+  },
+  {
+    "revision": "4f3da0b6eddf6854ba8640640fb437e4",
+    "url": "/wisielec/static/media/s9.4f3da0b6.jpg"
+  }
+]);
