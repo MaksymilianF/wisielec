f8b55c19aa0b848a5f7bf7b3ec7b5f6f3f332607
Max <max@dietr.net>
2020-06-05 14:11:10 +0200

    Add font UbuntuMono

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './fonts/UbuntuMono-Regular.ttf';
+import './fonts/UbuntuMono-Bold.ttf';
 
 ReactDOM.render(
   <React.StrictMode>
